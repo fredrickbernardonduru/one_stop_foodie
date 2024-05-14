a5f60622c18e327e4c0d2057db09fbeaeb1651af
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-05-14 11:26:39 +0300

    Button update

--- app/components/Common/Header.tsx
+++ app/components/Common/Header.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client"
 
 export const Header = () => {
   return (
-    <header>
-        
+    <header className="grid grid-cols-2 py-5 px-4 md:px-12 items-center sticky top-0 bg-white">
+        {/* Left Area */}
+        <div className="flex items-center gap-x-8">
+            <button className="p-2 rounded-full bg-slate-200 text-gray-500 hover:bg-orange-200 hover:text-green-600">
+
+            </button>
+
+        </div>
+
+        {/* Right Area */}
+
     </header>
   )
 }
